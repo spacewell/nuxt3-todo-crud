68eaee732095b7185d5a246fe152872c374aef1a
Magomed Makhmudov <mmriadovich@gmail.com>
2022-07-07 12:19:15 +0300

    init

---
+++ app.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <TheNavigation />
+    <NuxtPage />
+    <TheFooter />
+  </div>
+</template>
+
+<style>
+*, *::before, *::after {
+  padding: 0px;
+  margin: 0px;
+}
+</style>
---
+++ components/TheNavigation.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="container">
+    <h1>Work</h1>
+  </div>
+</template>
+
+<style scoped>
+
+.container {
+  width: 100%;
+  background: blue;
+  color: #fff;
+  padding: 20px;
+}
+
+</style>
